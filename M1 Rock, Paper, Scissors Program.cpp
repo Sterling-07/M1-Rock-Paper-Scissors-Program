80ef343794a8505215ea7811fb3784c08b026882
sterl <sterlingrodgers2007@gmail.com>
2026-02-11 12:47:05 -0500

    Add project files.

---
+++ M1 Rock, Paper, Scissors Program.cpp
@@ -0,0 +1,35 @@
+// M1 Rock, Paper, Scissors Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <random>
+using namespace std;
+
+void GetPlayerOption(int& option);
+
+int main()
+{
+	int option, choice;
+	random_device engine;
+	uniform_int_distribution<int> randomInt(1, 3);
+	choice = randomInt(engine);
+
+	GetPlayerOption(option);
+
+	return 0;
+}
+
+void GetPlayerOption(int& option)
+{
+	do
+	{
+		cout << "Please select your option: \n";
+		cout << "Option 1: Rock\n";
+		cout << "Option 2: Paper\n";
+		cout << "Option 3: Scissors\n";
+		cout << "Option 4: Quit\n";
+		cin >> option;
+	} while (option < 1 || option > 4);
+}
+
+
